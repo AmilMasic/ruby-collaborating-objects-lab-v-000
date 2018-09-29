265cf5ba9aa267f89681d30c49d9d8a92dd5eb1f
Amil Masic <amil.masic@gmail.com>
2018-09-29 00:10:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,13 @@
 class Song
 
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
 
   end
 
-  # def artist=(name)
-  #   self.artist = Artist.find_or_create_by_name(name)
-  #   # artist.add_song(self)
-  # end
+
 
 
 end
