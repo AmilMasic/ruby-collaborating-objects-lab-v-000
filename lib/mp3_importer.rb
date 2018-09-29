98a10b88a7b8abda32b61969423e229ca16b3e9a
Amil Masic <amil.masic@gmail.com>
2018-09-29 00:34:02 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -19,7 +19,6 @@ class MP3Importer
       # binding.pry
       self.files.each do |file|
         song = Song.new_by_filename(file)
-        # Artist.all << song.artist unless Artist.all.include?(song.artist)
       end
     end
 end
